43af67c81c6a5b28d37a42dcbba2ae1737c86771
Cyber_Gust <augusto.e.silva@hotmail.com>
2025-07-17 19:42:02 -0300

    seguro

--- painel-admin/next.config.ts
+++ painel-admin/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'qjhfmghkxaddvgfyzadm.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/produtos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default config
